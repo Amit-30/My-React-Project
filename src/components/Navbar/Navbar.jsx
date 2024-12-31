3d545f82e8fa959a383293669a1ad16d64102f47
amit <amitsaini2730@gmail.com>
2024-12-31 22:22:52 +0530

    update 1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,30 @@
-import React, { useEffect, useRef } from 'react'
-import './Navbar.css'
-import Logo from '../../assets/logo.png'
-import search_icon from '../../assets/search_icon.svg'
-import bell_icon from '../../assets/bell_icon.svg'
-import profile_img from '../../assets/profile_img.png'
-import caret_icon from '../../assets/caret_icon.svg'
-import { useNavigate } from 'react-router-dom'
-import { logout } from '../../firebase'
-
-
+import React, { useEffect, useRef } from "react";
+import "./Navbar.css";
+import Logo from "../../assets/logo.png";
+import search_icon from "../../assets/search_icon.svg";
+import bell_icon from "../../assets/bell_icon.svg";
+import profile_img from "../../assets/profile_img.png";
+import caret_icon from "../../assets/caret_icon.svg";
+import { logout } from "../../firebase";
 
 const Navbar = () => {
+  const navRef = useRef();
 
-  const navRef = useRef()
-
-  const navigate = useNavigate()
-
-  useEffect(()=>{
-    window.addEventListener('scroll', ()=>{
-      if (window.scrollY >= 80){
-        navRef.current.classList.add('nav-dark');
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY >= 80) {
+        navRef.current.classList.add("nav-dark");
       } else {
-        navRef.current.classList.remove('nav-dark');
+        navRef.current.classList.remove("nav-dark");
       }
-    })
-  },[])
+    });
+  }, []);
 
   return (
-    <div className='navbar' ref={navRef}>
-      <div className='navbar-left'>
+    <div className="navbar" ref={navRef}>
+      <div className="navbar-left">
         <img src={Logo} alt="logo" />
-        
+
         <ul>
           <li>Home</li>
           <li>TV Shows</li>
@@ -40,21 +34,21 @@ const Navbar = () => {
           <li>Browse by Languages</li>
         </ul>
       </div>
-      <div className='navbar-right'>
-        <img src={search_icon} alt="" className='icons'/>
+      <div className="navbar-right">
+        <img src={search_icon} alt="" className="icons" />
         <p>children</p>
-        <img src={bell_icon} alt="" className='icons'/>
-        
+        <img src={bell_icon} alt="" className="icons" />
+
         <div className="navbar-profile">
-          <img src={profile_img} alt="" className='profile'/>
+          <img src={profile_img} alt="" className="profile" />
           <img src={caret_icon} alt="" />
           <div className="dropdown">
-            <p onClick={()=>logout()}>Sign Out of Netflix</p>
+            <p onClick={logout}>Sign Out of Netflix</p>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
